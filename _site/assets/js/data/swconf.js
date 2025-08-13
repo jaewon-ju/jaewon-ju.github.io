7d29f7cafae2dd56f1b15c730a24a7a467fb40f1
Jaewon <20011211s@gmail.com>
2025-08-13 19:51:37 +0900

    [feat] change header css

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755078693',resources: [
+    cacheName: 'chirpy-1755082194',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
