a4022846ab505572e9e784a92ef898372ae0821e
jaewon-ju <jaewon-ju@users.noreply.github.com>
2025-08-13 08:45:29 +0000

    deploy: 19088f39d677abe99709cf28427ac3fb13c5e934

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755073110',resources: [
+    cacheName: 'chirpy-1755074728',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
