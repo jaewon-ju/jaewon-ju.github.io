8823a5a923477d908b1593e18a546db86a7ac007
Jaewon <20011211s@gmail.com>
2025-08-14 20:15:48 +0900

    [chore] avatar path relative -> absolute

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755082194',resources: [
+    cacheName: 'chirpy-1755169431',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
